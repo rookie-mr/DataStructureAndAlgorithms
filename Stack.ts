876b8f1f7b5ba0df7d353bd0adb1cce4e80fbab2
zfc <1264437217@qq.com>
2021-04-10 19:22:01 +0800

    test

--- Stack.ts
+++ Stack.ts
@@ -31,3 +31,46 @@ class Stack {
         console.log(this.items.toString())
     }
 }
+
+// 表达式求值
+/*
+形如 1+2*3-4/5 计算其输出
+*/
+
+function counter(s) {
+    const numberStack = new Array()
+    const operatorStack = new Array()
+    const higherPriority = ['*', '/']
+    let temp = ''
+    let status = 0 // 入栈状态
+    for (let i = 0; i < s.length; i++) {
+        if (Number(s[i])) {
+            temp += s[i]
+        } else {
+            numberStack.push(temp)
+            temp = ''
+            // 判断是否为待清除状态
+            if (status === 1) {
+                const l = operatorStack.length
+                let s = ''
+                for (let i = 0; i < l; i++) {
+                    s += numberStack.pop() + operatorStack.pop() + numberStack.pop()
+                    numberStack.push(eval(s))
+                }
+                status = 0
+            }
+
+            if (higherPriority.indexOf(s[i]) != -1) {
+                status = 1 // 待清除状态
+            }
+            operatorStack.push(s[i])
+        }
+    }
+    const l = operatorStack.length
+    let res = ''
+    for (let i = 0; i < l; i++) {
+        res += numberStack.pop() + operatorStack.pop() + numberStack.pop()
+        numberStack.push(eval(s))
+    }
+    return numberStack.pop()
+}
